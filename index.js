79b04d2fb7f1b5991d12a94880586d9758fe0f36
Amiel Ian C. Mendoza <amielmendoza143@gmail.com>
2023-08-22 22:39:16 +0800

    Change to class

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
-let myLibrary = [];
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  toggleReadButtons() {
+    this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleReadStatus = function () {
-  this.read = !this.read;
-};
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
-  displayBooks();
-}
+  addBook(title, author, pages, read) {
+    this.books.push(new Book(title, author, pages, read));
+  }
 
-function displayBooks() {
-  const bookContainer = document.querySelector(".books-container ul");
-  bookContainer.innerHTML = ""; //clear the container before iterating
+  removeBook(index) {
+    this.books.splice(index, 1);
+  }
 
-  const emptyText = document.querySelector(".empty-lib");
+  toggleReadStatus(index) {
+    this.books[index].toggleReadButtons();
+  }
 
-  if (myLibrary.length) {
-    emptyText.style.display = "none";
+  updateReadTotal() {
+    let read = 0;
+    let unread = 0;
 
-    for (let i = 0; i < myLibrary.length; i++) {
-      let book = myLibrary[i];
+    for (let book of this.books) {
+      if (book.read) {
+        read++;
+      } else {
+        unread++;
+      }
+    }
 
-      const readStatus = () => {
-        return book.read ? "read" : "unread";
-      };
+    const readTotal = document.querySelector(".books-read-num");
+    const unreadTotal = document.querySelector(".books-unread-num");
 
-      const bookEl = `<li>
+    readTotal.textContent = read;
+    unreadTotal.textContent = unread;
+  }
+
+  updateBookTotal() {
+    const bookTotal = document.querySelector(".book-total-num");
+    bookTotal.textContent = this.books.length;
+  }
+
+  displayBooks() {
+    const bookContainer = document.querySelector(".books-container ul");
+    bookContainer.innerHTML = ""; //clear the container before iterating
+
+    const emptyText = document.querySelector(".empty-lib");
+
+    if (this.books.length) {
+      emptyText.style.display = "none";
+
+      for (let i = 0; i < this.books.length; i++) {
+        let book = this.books[i];
+
+        const readStatus = () => {
+          return book.read ? "read" : "unread";
+        };
+
+        const bookEl = `<li>
       <div class="book" data-index="${i}">
         <div class="book-top">
           <i class="fa-solid fa-xmark book-remove" data-index="${i}"></i>
@@ -55,20 +91,47 @@ function displayBooks() {
       </div>
     </li>`;
 
-      const tempContainer = document.createElement("div");
-      tempContainer.innerHTML = bookEl;
+        const tempContainer = document.createElement("div");
+        tempContainer.innerHTML = bookEl;
 
-      bookContainer.appendChild(tempContainer.firstChild);
+        bookContainer.appendChild(tempContainer.firstChild);
+      }
+    } else {
+      emptyText.style.display = "block";
     }
-  } else {
-    emptyText.style.display = "block";
+
+    this.setupRemoveButtons();
+    this.setupToggleReadButtons();
+    this.updateBookTotal();
+    this.updateReadTotal();
   }
 
-  setupRemoveButtons();
-  setupToggleReadButtons();
-  updateBookTotal();
-  updateReadTotal();
+  setupRemoveButtons() {
+    const removeButtons = document.querySelectorAll(".book-remove");
+
+    for (let removeButton of removeButtons) {
+      removeButton.addEventListener("click", (event) => {
+        const index = event.target.dataset.index;
+        this.removeBook(index);
+        this.displayBooks();
+      });
+    }
+  }
+
+  setupToggleReadButtons() {
+    const toggleReadButtons = document.querySelectorAll(".book-status");
+
+    for (let toggleButton of toggleReadButtons) {
+      toggleButton.addEventListener("click", (event) => {
+        const index = event.target.dataset.index;
+        this.toggleReadStatus(index);
+        this.displayBooks();
+      });
+    }
+  }
 }
+
+const library = new Library();
 
 const addBookBtn = document.querySelector("#add-book-btn");
 const addBookForm = document.querySelector(".add-book-form");
@@ -80,11 +143,10 @@ closeFormBtn.addEventListener("click", () => {
 });
 
 addBookBtn.addEventListener("click", () => {
-  let formDisplay = addBookForm.style.display;
+  let formDisplay = getComputedStyle(addBookForm);
 
-  if (formDisplay === "none") {
+  if (formDisplay.display === "none") {
     addBookForm.style.display = "block";
-
     const titleInputForm = document.querySelector("#title-input");
     titleInputForm.focus();
   } else {
@@ -100,65 +162,12 @@ addBookForm.addEventListener("submit", (event) => {
   const pagesInput = document.querySelector("#pages-input").value;
   const readInput = document.querySelector("#read-input").checked;
 
-  addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
+  library.addBook(titleInput, authorInput, pagesInput, readInput);
 
   addBookForm.reset();
 
   addBookForm.style.display = "none";
+  library.displayBooks();
 });
 
-function setupRemoveButtons() {
-  const removeButtons = document.querySelectorAll(".book-remove");
-
-  for (let removeButton of removeButtons) {
-    removeButton.addEventListener("click", (event) => {
-      const index = event.target.dataset.index;
-      removeBook(index);
-      displayBooks();
-    });
-  }
-}
-
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-}
-
-function toggleReadStatus(index) {
-  myLibrary[index].toggleReadStatus();
-}
-
-function setupToggleReadButtons() {
-  const toggleReadButtons = document.querySelectorAll(".book-status");
-
-  for (let toggleButton of toggleReadButtons) {
-    toggleButton.addEventListener("click", (event) => {
-      const index = event.target.dataset.index;
-      toggleReadStatus(index);
-      displayBooks();
-    });
-  }
-}
-
-function updateReadTotal() {
-  let read = 0;
-  let unread = 0;
-
-  for (let book of myLibrary) {
-    if (book.read) {
-      read++;
-    } else {
-      unread++;
-    }
-  }
-
-  const readTotal = document.querySelector(".books-read-num");
-  const unreadTotal = document.querySelector(".books-unread-num");
-
-  readTotal.textContent = read;
-  unreadTotal.textContent = unread;
-}
-
-function updateBookTotal() {
-  const bookTotal = document.querySelector(".book-total-num");
-  bookTotal.textContent = myLibrary.length;
-}
+library.displayBooks();
